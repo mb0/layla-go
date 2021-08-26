dab09d83c0056e38199e9a6e44d7feeb31a4780d
Martin Schnabel <mb0@mb0.org>
2021-08-26 18:40:31 +0200

    layla: fix expected line height in tests

--- layla_test.go
+++ layla_test.go
@@ -22,26 +22,26 @@ func TestLayla(t *testing.T) {
 	}{
 		{`(stage w:360 h:360 (rect))`, `{kind:'rect' w:360 h:360}`},
 		{`(rect w:360 h:360 (text 'Hello'))`, `{kind:'rect' w:360 h:360}` +
-			`{kind:'text' w:79 h:41 font:{line:41} data:'Hello'}`},
+			`{kind:'text' w:79 h:40 font:{line:40} data:'Hello'}`},
 		{`(box w:360 h:360 (text 'Mr. A BC'))`,
-			`{kind:'text' w:136 h:41 font:{line:41} data:'Mr. A BC'}`},
+			`{kind:'text' w:136 h:40 font:{line:40} data:'Mr. A BC'}`},
 		{`(stage w:360 h:360 (rect h:100))`, `{kind:'rect' w:360 h:100}`},
 		{`(stage w:360 h:360 pad:[5 5 5 5] (rect h:100))`,
 			`{kind:'rect' x:5 y:5 w:350 h:100}`},
 		{`(stage w:360 h:360 pad:[5 5 5 5] (rect h:100 mar:[3 3 3 3]))`,
 			`{kind:'rect' x:8 y:8 w:344 h:100}`},
 		{`(markup w:360 "Test *Test* Test")`, `` +
-			`{kind:'text' w:65 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:73 w:66 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:147 w:65 h:41 font:{line:41} data:'Test'}`},
+			`{kind:'text' w:65 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:73 w:66 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:147 w:65 h:40 font:{line:40} data:'Test'}`},
 		{`(vbox w:150 pad:[1 1 1 1] (markup "Test *Test* Test"))`, `` +
-			`{kind:'text' x:1 y:1 w:65 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:74 y:1 w:66 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:1 y:42 w:65 h:41 font:{line:41} data:'Test'}`},
+			`{kind:'text' x:1 y:1 w:65 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:74 y:1 w:66 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:1 y:41 w:65 h:40 font:{line:40} data:'Test'}`},
 		{`(vbox w:300 pad:[26 0 26 0] (box w:300 mar:[50 1 50 1](markup "Test *Test* Test")))`, `` +
-			`{kind:'text' x:76 y:1 w:65 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:149 y:1 w:66 h:41 font:{line:41} data:'Test'}` +
-			`{kind:'text' x:76 y:42 w:65 h:41 font:{line:41} data:'Test'}`},
+			`{kind:'text' x:76 y:1 w:65 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:149 y:1 w:66 h:40 font:{line:40} data:'Test'}` +
+			`{kind:'text' x:76 y:41 w:65 h:40 font:{line:40} data:'Test'}`},
 		{`(vbox w:360 h:360 sub.h:36 (rect)(rect h:72)(rect))`, "" +
 			`{kind:'rect' w:360 h:36}` +
 			`{kind:'rect' y:36 w:360 h:72}` +
@@ -59,30 +59,30 @@ func TestLayla(t *testing.T) {
 			`(text 'a:') (text '1')` +
 			`(text 'b:') (text '2'))` +
 			`(text h:30 'end'))`, "" +
-			`{kind:'text' w:100 h:41 font:{line:41} data:'a:'}` +
-			`{kind:'text' x:100 w:200 h:41 font:{line:41} data:'1'}` +
-			`{kind:'text' y:41 w:100 h:41 font:{line:41} data:'b:'}` +
-			`{kind:'text' x:100 y:41 w:200 h:41 font:{line:41} data:'2'}` +
-			`{kind:'text' y:82 w:300 h:30 font:{line:41} data:'end'}`},
+			`{kind:'text' w:100 h:40 font:{line:40} data:'a:'}` +
+			`{kind:'text' x:100 w:200 h:40 font:{line:40} data:'1'}` +
+			`{kind:'text' y:40 w:100 h:40 font:{line:40} data:'b:'}` +
+			`{kind:'text' x:100 y:40 w:200 h:40 font:{line:40} data:'2'}` +
+			`{kind:'text' y:80 w:300 h:30 font:{line:40} data:'end'}`},
 		{`(vbox w:300 h:300 list:(list (text 'Hello') (text 'World')))`, "" +
-			`{kind:'text' w:300 h:41 font:{line:41} data:'Hello'}` +
-			`{kind:'text' y:41 w:300 h:41 font:{line:41} data:'World'}`},
+			`{kind:'text' w:300 h:40 font:{line:40} data:'Hello'}` +
+			`{kind:'text' y:40 w:300 h:40 font:{line:40} data:'World'}`},
 		{`(page w:200 h:41 (text 'Page3'))`, "" +
-			`{kind:'text' w:97 h:41 font:{line:41} data:'Page3'}`},
+			`{kind:'text' w:97 h:40 font:{line:40} data:'Page3'}`},
 		{`(page w:200 h:41 (vbox (text 'Page1') (text 'Page2') (text 'Page3')))`, "" +
-			`{kind:'text' w:200 h:41 font:{line:41} data:'Page1'}` +
-			`{kind:'page'}{kind:'text' w:200 h:41 font:{line:41} data:'Page2'}` +
-			`{kind:'page'}{kind:'text' w:200 h:41 font:{line:41} data:'Page3'}`},
+			`{kind:'text' w:200 h:40 font:{line:40} data:'Page1'}` +
+			`{kind:'page'}{kind:'text' w:200 h:40 font:{line:40} data:'Page2'}` +
+			`{kind:'page'}{kind:'text' w:200 h:40 font:{line:40} data:'Page3'}`},
 		{`(page w:200 h:41 (text 'Page1\nPage2\nPage3'))`, "" +
-			`{kind:'text' w:97 h:41 font:{line:41} data:'Page1'}` +
-			`{kind:'page'}{kind:'text' w:97 h:41 font:{line:41} data:'Page2'}` +
-			`{kind:'page'}{kind:'text' w:97 h:41 font:{line:41} data:'Page3'}`},
+			`{kind:'text' w:97 h:40 font:{line:40} data:'Page1'}` +
+			`{kind:'page'}{kind:'text' w:97 h:40 font:{line:40} data:'Page2'}` +
+			`{kind:'page'}{kind:'text' w:97 h:40 font:{line:40} data:'Page3'}`},
 		{`(page w:200 h:100 (text 'Page1\nPage2\nPage3'))`, "" +
-			`{kind:'text' w:97 h:82 font:{line:41} data:'Page1\nPage2'}` +
-			`{kind:'page'}{kind:'text' w:97 h:41 font:{line:41} data:'Page3'}`},
+			`{kind:'text' w:97 h:80 font:{line:40} data:'Page1\nPage2'}` +
+			`{kind:'page'}{kind:'text' w:97 h:40 font:{line:40} data:'Page3'}`},
 		{`(page w:200 h:41 (text 'Hello World\nHallo Welt'))`, "" +
-			`{kind:'text' w:176 h:41 font:{line:41} data:'Hello World'}` +
-			`{kind:'page'}{kind:'text' w:176 h:41 font:{line:41} data:'Hallo Welt'}`},
+			`{kind:'text' w:176 h:40 font:{line:40} data:'Hello World'}` +
+			`{kind:'page'}{kind:'text' w:176 h:40 font:{line:40} data:'Hallo Welt'}`},
 	}
 	reg := &lit.Reg{}
 	env := exp.Builtins(Specs(reg).AddMap(lib.Std))
@@ -124,12 +124,12 @@ func TestMeasure(t *testing.T) {
 	}{
 		{"(stage w:10 h:20)", `{"w":10,"h":20}`, ``},
 		{"(rect w:10 h:20)", `{"w":10,"h":20}`, ``},
-		{"(text 'Hello')", `{"w":79,"h":41}`, ``},
-		{"(text 'World')", `{"w":89,"h":41}`, ``},
-		{"(markup 'World')", `{"w":89,"h":41}`, ``},
-		{"(markup '*World*')", `{"w":90,"h":41}`, ``},
-		{"(text 'Hello World')", `{"w":89,"h":82}`, ``},
-		{"(text mar:[1 2 3 4] 'Hello')", `{"w":83,"h":47}`, `{"x":1,"y":2,"w":79,"h":41}`},
+		{"(text 'Hello')", `{"w":79,"h":40}`, ``},
+		{"(text 'World')", `{"w":89,"h":40}`, ``},
+		{"(markup 'World')", `{"w":89,"h":40}`, ``},
+		{"(markup '*World*')", `{"w":90,"h":40}`, ``},
+		{"(text 'Hello World')", `{"w":89,"h":80}`, ``},
+		{"(text mar:[1 2 3 4] 'Hello')", `{"w":83,"h":46}`, `{"x":1,"y":2,"w":79,"h":40}`},
 	}
 	reg := &lit.Reg{}
 	env := exp.Builtins(Specs(reg).AddMap(lib.Std))
